d3b4fe806159479cb153298b84f92674b8b1ab19
ariel <arielesquivelpcmail.com>
2023-11-02 18:23:02 -0300

    servidor funcionando

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
 //hola
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+app.listen(3001, () => {
+  console.log("servidor en puerto 3001");
+});
